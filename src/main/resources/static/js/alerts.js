72d8e1d61f191b6823666fd81473dace9e42a3a4
PrasannaVenkatasubramaniansc <prasanna@snapchat.com>
2018-03-18 23:11:03 -0700

    Get the alert working the second time as well

--- src/main/resources/static/js/alerts.js
+++ src/main/resources/static/js/alerts.js
@@ -10,21 +10,18 @@ var alerts = (function() {
     function showMsg(message, kind, timeout) {
         var messageClass = `.${kind}Message`;
 
-        $(`#messageDiv ${messageClass} .message`).html(message)
+        $(`#messageDiv ${messageClass} .message`).html(message);
         // Notes on fadeIn:
         // Relying on css transition for fade in and out didn't work even after following: http://stackoverflow.com/a/13257654/7247103
         // Using jquery based fadeOut works but I haven't figured the right place to put the fadeIn as we seem to prepend the message
         // over existing content.
-        $("#alertMessage").append($(`#messageDiv ${messageClass}`).clone())
+        $("#alertMessage").append($(`#messageDiv ${messageClass}`).clone());
+        $("#alertMessage").show();
 
         setTimeout(function() {
             $("#alertMessage").fadeOut();
+            $("#alertMessage").html('');
         }, timeout);
-    }
-
-    function removeMsg() {
-        $(".messageSection .successMessage").remove();
-        $(".messageSection .warningMessage").remove();
     }
 
     return {
